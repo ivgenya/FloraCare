b1ea218cf602717cbb23e94cff0f101779b96e9d
Evgeniya Malinina <evgeniyamalinina0707@gmail.com>
2024-11-20 21:53:50 +0300

    Изменена структура базы данных

--- db/database.sql
+++ db/database.sql
@@ -1,45 +1,49 @@
 -- CREATE DATABASE flora_care;
 
 CREATE TABLE user (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id BINARY(16) NOT NULL PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     email VARCHAR(255) UNIQUE,
     password_hash VARCHAR(255),
     google_id VARCHAR(255),
+    avatar_url VARCHAR(255),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE room (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id BINARY(16) NOT NULL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    user_id INT NOT NULL,
-    image BLOB,
+    user_id BINARY(16) NOT NULL,
+    image VARCHAR(255),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
 );
 
 CREATE TABLE plant (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    room_id INT,
+    id BINARY(16) NOT NULL PRIMARY KEY,
+    room_id BINARY(16),
     name VARCHAR(255) NOT NULL,
     min_temperature INT,
     max_temperature INT,
     min_humidity INT,
     max_humidity INT,
+    watering_frequency INT,
+    fertilizing_frequency INT,
+    description TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (room_id) REFERENCES room(id) ON DELETE CASCADE
 );
 
 CREATE TABLE plant_photo (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    plant_id INT,
-    image BLOB,
+    id BINARY(16) NOT NULL PRIMARY KEY,
+    plant_id BINARY(16),
+    image VARCHAR(255),
     FOREIGN KEY (plant_id) REFERENCES plant(id) ON DELETE CASCADE
 );
 
 CREATE TABLE device (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    room_id INT NOT NULL,
+    id BINARY(16) NOT NULL PRIMARY KEY,
+    room_id BINARY(16) NOT NULL,
     name VARCHAR(255) NOT NULL,
     mac_address VARCHAR(255) UNIQUE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -47,10 +51,9 @@ CREATE TABLE device (
 );
 
 CREATE TABLE schedule (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    plant_id INT,
+    id BINARY(16) NOT NULL PRIMARY KEY,
+    plant_id BINARY(16),
     watering_date DATE,
     is_watered BOOLEAN DEFAULT FALSE,
     FOREIGN KEY (plant_id) REFERENCES plant(id) ON DELETE CASCADE
 );
-
